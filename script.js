5caf11c8eaddb655520dd1deb6b1a81b14aac366
kevinsimms <58520326+kevinsimms@users.noreply.github.com>
2023-10-20 11:52:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,181 @@
+function check(){
+
+
+    var bod=document.body;
+    bod.style.backgroundColor="black";
+    
+     var photon1=document.getElementById("photo");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("photo2");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("photo3");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("photo4");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("photo5");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("photo6");
+     photon1.style.display="none";
+     var photon1=document.getElementById("photo7");
+     photon1.style.display="none";
+
+     var photon1=document.getElementById("check");
+     photon1.style.display="none";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="block";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="block";
+
+
+}
+
+
+function check2(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    
+     var photon1=document.getElementById("photo");
+     photon1.style.display="block";
+
+     var photon1=document.getElementById("check");
+     photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+
+}
+
+
+function checktree(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo2");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
+function checkmont(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo3");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
+function checkaff(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo4");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
+function checkball(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo5");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
+function checklac(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo6");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
+function pigeon(){
+
+    var bod=document.body;
+    bod.style.backgroundColor="white";
+
+    var photon1=document.getElementById("photo7");
+    photon1.style.display="block";
+
+    var photon1=document.getElementById("check");
+    photon1.style.display="block";
+
+var allphoto=document.getElementById("photoensemble");
+allphoto.style.display="none";
+
+var allphoto=document.getElementById("photoensemble2");
+allphoto.style.display="none";
+
+}
+
+
